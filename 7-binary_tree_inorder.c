406476862c7f1017724c61b0397802065f765141
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-11-03 02:08:08 +0200

    Update 7-binary_tree_inorder.c

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that creates a binary tree node
+ * inorder - function creates a binary tree node
  * @tree: pointer to the parent node of the node to create
  * @func: pointer of function
- * Return: pointer to the new node, or NULL on failure
+ * Return: pointer to the new node
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-
 	binary_tree_inorder(tree->left, func);
 	func(tree->n);
 	binary_tree_inorder(tree->right, func);
