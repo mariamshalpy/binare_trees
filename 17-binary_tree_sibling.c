69974adf630b358a01c9eb4ef22b13a47bcf272e
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-11-03 02:11:32 +0200

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - finds the sibling of a node
+ * sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling of
- *
  * Return: pointer to the sibling node, or NULL if no sibling
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
@@ -12,5 +11,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	if (node == node->parent->left)
 		return (node->parent->right);
+	
 	return (node->parent->left);
 }
