06b5d465cf1bab899edfd060f44d2c49a7c9b88f
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-11-03 02:11:02 +0200

    Update 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to the node to find the uncle of
- *
  * Return: pointer to the uncle node, or NULL if no uncle
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
@@ -12,5 +11,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 		return (NULL);
 	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
+	
 	return (node->parent->parent->left);
 }
