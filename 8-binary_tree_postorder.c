8df4702b9598c9b04e208f2ccd773af9fa4552f9
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-11-03 02:07:41 +0200

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - function that creates a binary tree node
+ * postorder - function creates tree node
  * @tree: pointer to the parent node of the node to create
  * @func: pointer of function
  * Return: pointer to the new node, or NULL on failure
@@ -10,7 +10,6 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
-
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
