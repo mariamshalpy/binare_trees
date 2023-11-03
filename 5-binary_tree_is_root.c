42fa7701848b1b1504cbcdffe1c6c5c1011435a0
Mariam Shalaby <98620529+mariamshalpy@users.noreply.github.com>
2023-11-03 02:08:53 +0200

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,15 +1,14 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - function that creates a binary tree node
+ * is_root - function creates a binary tree node
  * @node: pointer to the parent node of the node to create
- * Return: pointer to the new node, or NULL on failure
+ * Return: pointer to the new node
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-
 	if (node->parent != NULL)
 		return (0);
 	else
